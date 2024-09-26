5701bb16a399393bf12066c85eddf28b5418923d
Grace Ikechi <mgbechigrace08@gmail.com>
2024-09-26 07:11:38 -0400

    add HelloWorld

---
+++ src/main/java/com/bptn/course/hello/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.bptn.course.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		System.out.println("Hello World222222");
+		
+		System.out.println("Hello World3333332");
+
+
+	}
+	
+	
+
+}
